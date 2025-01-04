0186d0e0375134c0bb840052f125d6d2bfd32a8f
m1m1sha <18262227804@163.com>
2025-01-04 20:37:35 +0800

    build: 更新依赖版本并修复配置文件

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,7 +1,7 @@
+import type { UseModule } from '~/types/modules'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
-import type { UseModule } from '~/types/modules'
 
 export const install: UseModule = (app) => {
   const router = createRouter({
