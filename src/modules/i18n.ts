a1e2320cd1826306e78b6c4b66ee2d6022c1f314
m1m1sha <18262227804@163.com>
2026-01-04 18:28:40 +0800

    chore: update bun.lock and i18n.ts for configuration adjustments

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -4,11 +4,9 @@ import { createI18n } from 'vue-i18n'
 
 // Import i18n resources
 // https://vitejs.dev/guide/features.html#glob-import
-//
-// Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
 const i18n = createI18n({
   legacy: false,
-  locale: 'zh-CN',
+  locale: '',
   messages: {},
 })
 
